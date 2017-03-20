091d2185d47a7fca3319ccaf55cc104f8316ae6b
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-21 00:21:41 +0530

    Create swapTwoNumbers.java
    Swap two numbers without using third variable

---
+++ swapTwoNumbers.java
@@ -0,0 +1,17 @@
+class swapTwoNumbers
+{
+	public static void main(String args[])
+	{
+		int x = 10;
+		int y = 20;
+		
+		x = x + y; // 30
+		y = x - y; // 10
+		x = x - y; // 20
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+	}
+		
+}
